09c7e3dd35f72fa28d957e3baea2150c699112e6
Lizzy2653 <147071082+Lizzy2653@users.noreply.github.com>
2023-10-13 16:27:37 -0400

    uploading donut-shop

---
+++ donut-shop/Donut.java
@@ -0,0 +1,22 @@
+public class Donut {
+}
+    public static void main (String)[] args)
+        String type;
+        int calories;
+        double price;
+        String description;
+
+    myDonuts(0) = newDonut
+    myDonut(1) = newDonut; 250, 1.50, ("Glazed Donut");
+    myDonut(2) = newDonut; 300, 2.00, ("Chocolate Frosted");
+    myDonut(3) = newDonut; 200, 1.50, ("Boston Cream");
+    myDonut(4) = newDonut; 150, 1.00, ("Strawberry");
+    myDonut(5) = newDonut; 300, 2.00, ("Rasperry Filling");
+    myDonut(6) = newDonut; 100, 1.00  ("Old Fashioned");
+
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+    System.out.println("4");
+    System.out.println("5");
+    System.out.println("6");
